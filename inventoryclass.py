a35f9fa51afb233aaa9cd65582ac9266ddbc69c8
filip.lezuch <filiplez18@gmail.com>
2019-04-06 15:13:39 +0200

    technical description of all methods of Inventory class

--- inventoryclass.py
+++ inventoryclass.py
@@ -1,12 +1,16 @@
 # to create Inventory class object simply:
 # bag = Inventory()
 
-# in order to print whole existing inventory simply :
-# bag.print()
-
 # to add space to an existing inventory use:
 # bag.inventory_space_add(x)
 # where x is an amount of bracket you want to add
+
+# to check whether there is enough space in inventory use:
+# bag.inventory_space_check() method
+
+# to check an index of a particular item use:
+# bag.item_place(item) method
+# where item is an item you are looking for a place of
 
 # to add an item to an existing inventory use:
 # bag.add_to_inventory('Wood', 10)
@@ -15,6 +19,18 @@
 # to subtract an item to an existing inventory use:
 # bag.subtract_from_inventory('Slime', 5)
 # where 'Slime' is an item from which you want to subtract 5 instances
+
+# to check whether item exists in inventory use:
+# bag.check_if_item_exists_in_inventory(search_item)
+# where search_item is an item you are looking for
+
+# to check whether amount of specific item is in inventory use:
+# bag.check_item_amount_in_inventory(search_item, search_amount)
+# where search_item is an item you are looking for and search_amount is the amount you are requesting to check
+# note that this method first calls check_if_item_exists_in_inventory method
+
+# to print whole inventory use:
+# bag.print()
 
 
 class Inventory:
@@ -36,7 +52,7 @@ class Inventory:
             print('Enough space in inventory!')
             return True
         else:
-            print('Inventory full')
+            print('Inventory is full')
             return False
 
     def item_place(self, item):
@@ -61,6 +77,13 @@ class Inventory:
             if self.inventory_amount[place] == 0:  # if item's count is 0 then reset bracket
                 self.inventory_item[place] = 'empty'
 
+    def check_if_item_exists_in_inventory(self, search_item):
+        if search_item in self.inventory_item:
+            return True
+        else:
+            print('No {} in inventory.'.format(search_item))
+            return False
+
     def check_item_amount_in_inventory(self, search_item, search_amount):
         if self.check_if_item_exists_in_inventory(search_item):
             i = self.item_place(search_item)
@@ -71,13 +94,6 @@ class Inventory:
                 print('Not enough {} in inventory, you need {} more.'.format(search_item, var_1))
                 return False
         else:
-            return False
-
-    def check_if_item_exists_in_inventory(self, search_item):
-        if search_item in self.inventory_item:
-            return True
-        else:
-            print('No {} in inventory.'.format(search_item))
             return False
 
     def print(self):
